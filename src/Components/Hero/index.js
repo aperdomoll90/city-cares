a91ed85e6bd803b3f5cf38c6d7273b97553a474e
Shashank Kumar Mishra <Shashank@CITY-FURNITURE.com>
2022-10-08 13:17:25 -0400

    Added Upload button Functionality

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadButton from "../UploadButton";
 import "./styles.css";
 const heroTitle = require("../../Media/heroTitle.png");
@@ -8,21 +8,20 @@ const cloudTwo = require("../../Media/cloudTwo.png");
 const cloudThree = require("../../Media/cloudThree.png");
 
 export default function Hero() {
+
   return (
-      <section id='hero-wrapper'>
-        <img className='hero-title' src={heroTitle} alt='Hero Title' />
-                <p className='hero-comment'>Upload a picture to see where should your waste go!</p>
+    <section id='hero-wrapper'>
+      <img className='hero-title' src={heroTitle} alt='Hero Title' />
+      <p className='hero-comment'>Upload a picture to see where should your waste go!</p>
       <div className='hero-image-container'>
         <img className='planet' src={planet} alt='3d cartoon planet' />
         <img className='cloudOne' src={cloudOne} alt='3d cloud' />
         <img className='cloudTwo' src={cloudTwo} alt='3d cloud' />
         <img className='cloudThree' src={cloudThree} alt='3d cloud' />
-          </div>
-          <UploadButton/>
-
-          <div className="logo">
-
-          </div>
+      </div>
+      <UploadButton/>
+      <div className="logo">
+      </div>
     </section>
   );
 }
