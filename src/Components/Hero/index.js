2e3b96f2de8c5ed68ccc8f1c6117f44fb0d5a755
Juan Yepes <yepes.j@hotmail.com>
2022-10-08 15:51:28 -0400

    fix conflicts

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import UploadButton from '../UploadButton'
-import './styles.css'
-const heroTitle = require('../../Media/heroTitle.png')
-const planet = require('../../Media/planet.png')
-const cloudOne = require('../../Media/cloudOne.png')
-const cloudTwo = require('../../Media/cloudTwo.png')
-const cloudThree = require('../../Media/cloudThree.png')
-const bear = require('../../Media/citycarebear.png')
+import React, { useEffect, useState } from "react";
+import UploadButton from "../UploadButton";
+import "./styles.css";
+const heroTitle = require("../../Media/heroTitle.png");
+const planet = require("../../Media/planet.png");
+const cloudOne = require("../../Media/cloudOne.png");
+const cloudTwo = require("../../Media/cloudTwo.png");
+const cloudThree = require("../../Media/cloudThree.png");
+const bear = require("../../Media/citycarebear.png");
 
 export default function Hero() {
-  const [modalInfo, setModalInfo] = useState([])
-  const [openModal, setOpenModal] = useState(false)
+  const [modalInfo, setModalInfo] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
 
   return (
-<<<<<<< HEAD
     <section id="hero-wrapper">
       <img className="hero-title" src={heroTitle} alt="Hero Title" />
       <p className="hero-comment">
@@ -25,31 +24,17 @@ export default function Hero() {
         <img className="cloudTwo" src={cloudTwo} alt="3d cloud" />
         <img className="cloudThree" src={cloudThree} alt="3d cloud" />
       </div>
-      <UploadButton />
-
-      <div className="logo"></div>
-=======
-    <section id='hero-wrapper'>
-      <img className='hero-title' src={heroTitle} alt='Hero Title' />
-      <p className='hero-comment'>Upload a picture to see where should your waste go!</p>
-      <div className='hero-image-container'>
-        <img className='planet' src={planet} alt='3d cartoon planet' />
-        <img className='cloudOne' src={cloudOne} alt='3d cloud' />
-        <img className='cloudTwo' src={cloudTwo} alt='3d cloud' />
-        <img className='cloudThree' src={cloudThree} alt='3d cloud' />
-      </div>
       <UploadButton setOpenModal={setOpenModal} setModalInfo={setModalInfo} />
       {openModal === true && (
-        <div className='modal-wrapper'>
-          <div className='modal-container'>
-            <div className='close-button' onClick={() => setOpenModal(false)}>
+        <div className="modal-wrapper">
+          <div className="modal-container">
+            <div className="close-button" onClick={() => setOpenModal(false)}>
               X
             </div>
-            <img className='modal-bear' src={bear} alt='3d cloud' />
+            <img className="modal-bear" src={bear} alt="3d cloud" />
           </div>
         </div>
       )}
->>>>>>> f56917fdf0ac9334117c421c9753e24c84ecb9f3
     </section>
-  )
+  );
 }
--- src/Components/UploadButton/index.js
+++ src/Components/UploadButton/index.js
@@ -1,139 +1,117 @@
-<<<<<<< HEAD
-import React from "react";
-import styles from "./styles.css";
-export default function UploadButton() {
-  return (
-    <div id="UploadButton">
-      <svg
-        width="47"
-        height="62"
-        viewBox="0 0 47 62"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g filter="url(#filter0_d_4_76)">
-          <path
-            d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z"
-            fill="white"
-          />
-          <path
-            d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z"
-            stroke="white"
-          />
-        </g>
-        <defs>
-          <filter
-            id="filter0_d_4_76"
-            x="0.0925293"
-            y="0.393707"
-            width="46.6218"
-            height="60.6615"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
-          >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="4" />
-            <feGaussianBlur stdDeviation="2" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_4_76"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_4_76"
-              result="shape"
-            />
-          </filter>
-        </defs>
-      </svg>
-=======
 import React, { useState } from "react";
-import styles from './styles.css'
-export default function UploadButton({setModalInfo,setOpenModal}) {
+import styles from "./styles.css";
+export default function UploadButton({ setModalInfo, setOpenModal }) {
   const [imageData, setInput] = useState();
 
   function uploadImage(e) {
     const { files } = e.target.files[0];
     setInput(files);
-    console.log(imageData)
-    setInput(imageData)
-    handleSubmitFile()
+    console.log(imageData);
+    setInput(imageData);
+    handleSubmitFile();
   }
 
-    // Image/File Submit Handler
-    const handleSubmitFile = () => {
-      if (imageData !== null) {
-        var data = new FormData();
-        data.append("data", imageData);
-    
-        fetch("http://localhost:8910/taskCreationController/createStoryTask", {
-          method: "POST",
-          headers: {
-            "Content-Type": "multipart/form-data",
-            "Accept": "application/json",
-            "type": "formData"
-          },
-          body: data
-        }).then(function (res) {
+  // Image/File Submit Handler
+  const handleSubmitFile = () => {
+    if (imageData !== null) {
+      var data = new FormData();
+      data.append("data", imageData);
+
+      fetch("http://localhost:8910/taskCreationController/createStoryTask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Accept: "application/json",
+          type: "formData",
+        },
+        body: data,
+      }).then(
+        function (res) {
           if (res.ok) {
             alert("Perfect! ");
-            setModalInfo(imageData)
-            setOpenModal(true)
-        
+            setModalInfo(imageData);
+            setOpenModal(true);
           } else if (res.status === 401) {
             alert("Oops! ");
           }
-        }, function (e) {
-          setModalInfo(imageData)
+        },
+        function (e) {
+          setModalInfo(imageData);
           // setModalInfo('images test')
-          setOpenModal(true)
-      
-        });
-    
-      }
+          setOpenModal(true);
+        }
+      );
     }
-  
-  
+  };
 
   return (
-    <div id='UploadButton' >
+    <div id="UploadButton">
       <label for="file-input">
-        <svg width="47" height="62" viewBox="0 0 47 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="47"
+          height="62"
+          viewBox="0 0 47 62"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g filter="url(#filter0_d_4_76)">
-            <path d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z" fill="white" />
-            <path d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z" stroke="white" />
+            <path
+              d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z"
+              fill="white"
+            />
+            <path
+              d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z"
+              stroke="white"
+            />
           </g>
           <defs>
-            <filter id="filter0_d_4_76" x="0.0925293" y="0.393707" width="46.6218" height="60.6615" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <filter
+              id="filter0_d_4_76"
+              x="0.0925293"
+              y="0.393707"
+              width="46.6218"
+              height="60.6615"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
               <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
               <feOffset dy="4" />
               <feGaussianBlur stdDeviation="2" />
               <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_76" />
-              <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_76" result="shape" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_4_76"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_4_76"
+                result="shape"
+              />
             </filter>
           </defs>
         </svg>
         <span>
-          <input id="file-input" type="file" accept="image/*" multiple={false} onChange={uploadImage} />
+          <input
+            id="file-input"
+            type="file"
+            accept="image/*"
+            multiple={false}
+            onChange={uploadImage}
+          />
         </span>
       </label>
->>>>>>> f56917fdf0ac9334117c421c9753e24c84ecb9f3
     </div>
   );
 }
