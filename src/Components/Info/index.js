b463d87acf4ce5e3a2936d24bbf87907a6695d25
Juan Yepes <yepes.j@hotmail.com>
2022-10-08 10:51:20 -0400

    info sec

--- src/Components/Info/index.js
+++ src/Components/Info/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function Index() {
-  return <div className="info-section">index</div>;
+  return (
+    <>
+      <div className="info-section">
+        <div className="grid-wrapper">
+          <div class="one">One</div>
+          <div class="two">Two</div>
+          <div class="three">Three</div>
+          <div class="four">Four</div>
+          <div class="five">Five</div>
+        </div>
+      </div>
+    </>
+  );
 }
