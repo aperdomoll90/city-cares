a565fd78c1fc89192714b5039918a2c019e26363
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2022-10-08 18:20:56 -0400

    modal for video fixed

---
+++ src/Media/rockLine.js
@@ -0,0 +1,26 @@
+export const rockLine = <svg
+  width="1501"
+  height="380"
+  viewBox="0 0 1501 380"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+    d="M73.5 155.5L-18 108.5V412H1496.5L1500 171.5L1470 176L1457 196.5L1430 222L1392.5 227.5L1384 218.5L1371.5 171.5H1342.5L1316 238H1272.5L1233.5 227.5L1226.5 208.5L1192 200.5L1178.5 218.5L1158.5 227.5L1091.5 222L1066 208.5L1046 171.5V126.5L965.5 143.5V188.5H897.5L878.5 137.5H838.5L803.5 214L749.5 208.5L711.5 171.5L693.5 137.5L675 126.5L647 143.5H626.5L608.5 137.5L592.5 108.5H558.5L539 126.5L481 120L430 1L394 6L361.5 25.5L328.5 108.5L270 103.5L237.5 108.5L190.5 171.5H140L111.5 133.5H87L73.5 155.5Z"
+    fill="url(#paint0_radial_4_39)"
+    stroke="#240A18"
+  />
+    <defs>
+    <radialGradient
+      id="paint0_radial_4_39"
+      cx="0"
+      cy="0"
+      r="1"
+      gradientUnits="userSpaceOnUse"
+      gradientTransform="translate(727 528) rotate(-90.33) scale(434.007 1602.98)"
+    >
+    <stop />
+    <stop offset="1" stop-color="#330D1F" />
+    </radialGradient>
+    </defs>
+    </svg>
