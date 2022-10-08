288b8744a680715bbd6b57399333c9c11b59df3c
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2022-10-08 14:03:45 -0400

    modal added

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,28 +1,35 @@
-import React from "react";
-import UploadButton from "../UploadButton";
-import "./styles.css";
-const heroTitle = require("../../Media/heroTitle.png");
-const planet = require("../../Media/planet.png");
-const cloudOne = require("../../Media/cloudOne.png");
-const cloudTwo = require("../../Media/cloudTwo.png");
-const cloudThree = require("../../Media/cloudThree.png");
+import React, { useState } from 'react'
+import UploadButton from '../UploadButton'
+import './styles.css'
+const heroTitle = require('../../Media/heroTitle.png')
+const planet = require('../../Media/planet.png')
+const cloudOne = require('../../Media/cloudOne.png')
+const cloudTwo = require('../../Media/cloudTwo.png')
+const cloudThree = require('../../Media/cloudThree.png')
+const bear = require('../../Media/citycarebear.png')
 
 export default function Hero() {
+    const [openModal, setOpenModal] = useState(false)
+
   return (
-      <section id='hero-wrapper'>
-        <img className='hero-title' src={heroTitle} alt='Hero Title' />
-                <p className='hero-comment'>Upload a picture to see where should your waste go!</p>
+    <section id='hero-wrapper'>
+      <img className='hero-title' src={heroTitle} alt='Hero Title' />
+      <p className='hero-comment'>Upload a picture to see where should your waste go!</p>
       <div className='hero-image-container'>
         <img className='planet' src={planet} alt='3d cartoon planet' />
         <img className='cloudOne' src={cloudOne} alt='3d cloud' />
         <img className='cloudTwo' src={cloudTwo} alt='3d cloud' />
         <img className='cloudThree' src={cloudThree} alt='3d cloud' />
+      </div>
+      <UploadButton setOpenModal={setOpenModal} />
+      {openModal===true && (
+        <div className='modal-wrapper'> hello
+          <div className='modal-container'>
+            <div className='close-button' onClick={() => setOpenModal(false)}>X</div>
+            <img className='modal-bear' src={bear} alt='3d cloud' />
           </div>
-          <UploadButton/>
-
-          <div className="logo">
-
-          </div>
+        </div>
+      )}
     </section>
-  );
+  )
 }
--- src/Components/UploadButton/index.js
+++ src/Components/UploadButton/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './styles.css'
-export default function UploadButton() {
+export default function UploadButton({setOpenModal}) {
   return (
-      <div id='UploadButton'>
+      <div id='UploadButton' onClick={()=>setOpenModal(true)}>
           <svg width="47" height="62" viewBox="0 0 47 62" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_d_4_76)">
 <path d="M30.4069 52.5552H16.169C15.0644 52.5552 14.169 51.6597 14.169 50.5552V29.531C14.169 28.4265 13.2736 27.531 12.169 27.531H6.59682C4.97139 27.531 4.02482 25.6949 4.96769 24.3709L21.0882 1.73358C21.8666 0.640487 23.4794 0.608978 24.2999 1.67082L41.7924 24.3081C42.8082 25.6228 41.8712 27.531 40.2098 27.531H34.4069C33.3024 27.531 32.4069 28.4265 32.4069 29.531V50.5552C32.4069 51.6597 31.5115 52.5552 30.4069 52.5552Z" fill="white"/>
