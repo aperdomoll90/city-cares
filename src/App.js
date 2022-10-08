148329c9d5a30353fdaf0341cadef0776a895129
Juan Yepes <yepes.j@hotmail.com>
2022-10-08 10:23:00 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
-import './App.css';
-import Hero from './Components/Hero';
+import "./App.css";
+import Hero from "./Components/Hero";
+import Info from "./Components/Info";
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
+      <Hero />
+      <Info />
     </div>
-  )
+  );
 }
 
 export default App;
